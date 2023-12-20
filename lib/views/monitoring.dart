2f97a7c9fd0b5d585f719a103c8fd7526411b6d6
DevelopmenSmartProcess <smartprocessgroup2020@gmail.com>
2023-12-20 18:09:14 -0500

    Bases del monitoring

--- lib/views/monitoring.dart
+++ lib/views/monitoring.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:my_app/Colors/colors.dart';
 import 'package:my_app/components/app_bar.dart';
+import 'package:my_app/components/box_menu.dart';
 import 'package:my_app/services/wifi_services.dart';
 
 class Monitoring extends StatefulWidget {
@@ -55,51 +56,90 @@ class _MonitoringState extends State<Monitoring> {
     } else if (module.contains('M3')) {
       moduleCommand = '[OM]';
     }
-
+    module = module.substring(1,4).trim();
+    
     return Scaffold(
       appBar: AppBarCustom(
         tittle: 'Monitoring $module',
       ),
       body: Container(
         margin: const EdgeInsets.only(top: 15),
-        //====================Take Messures Button====================//
-        child:  Padding(  
-          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),   
-          child: Ink(
-            height: 50,
-            decoration: const BoxDecoration(
-              color: AppColors.secondColor,
-              borderRadius: BorderRadius.all(Radius.circular(25))
-            ),
-            child: InkWell(
-              onTap: () => startTimer(moduleCommand),
-              borderRadius: const BorderRadius.all(Radius.circular(25)),
-              splashColor: Colors.grey,
-              child: Align(
-                alignment: Alignment.center,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      height: 30,
-                      width: 30,
-                      child: Image.asset(
-                        'assets/tomar_medidas.jpg',
-                        alignment: Alignment.centerRight,
-                      ),
-                    ),
-                    const Text(
-                      'Take messures',
-                      style: TextStyle(
-                        color: AppColors.fontSecond,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ],
+        
+        child:  Column(
+          children: [
+            //====================Take Messures Button====================//
+            Padding(  
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),   
+              child: Ink(
+                height: 50,
+                decoration: const BoxDecoration(
+                  color: AppColors.secondColor,
+                  borderRadius: BorderRadius.all(Radius.circular(25))
                 ),
-              ),
+                child: InkWell(
+                  onTap: () => startTimer(moduleCommand),
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                  splashColor: Colors.grey,
+                  child: Row(                
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 70,
+                        child: Image.asset(
+                          'assets/tomar_medidas.jpg',
+                          alignment: Alignment.center,
+            
+                        ),
+                      ),
+                      const Expanded(
+                        child: Text(
+                          'Take messures',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: AppColors.fontSecond,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+            
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 75),
+                    ],
+                  ),
+                ),
+              )
             ),
-          )
+            
+            // ====================List of Measurements==================== //
+        Expanded(
+          child: ListView(
+            shrinkWrap: true, // Esto permite que el ListView se ajuste a su contenido
+            children: const [
+              // Frequency
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Voltages
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Currents
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Apparent Powers
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Active Powers
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Reactive Power
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Power Factor
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Harmonic Voltages
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              //Harmonic Currents
+              BoxMenu(icon: Icons.abc, tittleBox: 'tittleBox', margins: EdgeInsets.all(12)),
+              
+            
+            ],
+          ),
+        ),
+            
+          ],
         ),
       ),
     );
